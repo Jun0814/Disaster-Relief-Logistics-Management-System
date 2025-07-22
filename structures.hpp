eedde39780b45a826706c1cca19b573db6e8cefc
Jun0814 <jiajunchong00@gmail.com>
2025-07-22 15:16:37 +0800

    path update

--- structures.hpp
+++ structures.hpp
@@ -8,11 +8,11 @@
 using namespace std;
 
 // Global file names
-string fileSupplyBox = "../database/SupplyBox.csv";
-string fileVolunteer = "../database/Volunteer.csv";
-string fileEmergencyRequest = "../database/EmergencyRequest.csv";
-string fileSupplyRequest = "../database/SupplyRequest.csv";
-string fileVolunteerRequest = "../database/VolunteerRequest.csv";
+string fileSupplyBox = "database/SupplyBox.csv";
+string fileVolunteer = "database/Volunteer.csv";
+string fileEmergencyRequest = "database/EmergencyRequest.csv";
+string fileSupplyRequest = "database/SupplyRequest.csv";
+string fileVolunteerRequest = "database/VolunteerRequest.csv";
 string fileVolunteerAssignment = "database/VolunteerAssignment.csv";
 string fileVehicle = "database/Vehicle.csv";
 string fileTransportOrder = "database/TransportOrder.csv";
